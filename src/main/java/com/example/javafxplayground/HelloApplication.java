daf47c8297289de57c7962973c0a523056b77039
Louis Agyapong <47106199+louis-agyapong@users.noreply.github.com>
2022-11-28 15:29:35 +0000

    Extract and validate

--- src/main/java/com/example/javafxplayground/HelloApplication.java
+++ src/main/java/com/example/javafxplayground/HelloApplication.java
@@ -6,8 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.text.Text;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
@@ -25,38 +24,42 @@ public class HelloApplication extends Application {
         window = stage;
         window.setTitle("JavaFX - Title");
 
-        GridPane grid = new GridPane();
-        grid.setPadding(new Insets(10, 10, 10, 10));
-        grid.setVgap(8);
-        grid.setHgap(8);
-
-        // Name label
-        Label nameLabel = new Label("Username:");
-        GridPane.setConstraints(nameLabel, 0, 0);
-
-        // Name field
+        // Form
         TextField nameInput = new TextField();
-        GridPane.setConstraints(nameInput, 1, 0);
+        Button button = new Button("Click me");
+        Label label = new Label();
+        button.setOnAction(e -> {
+            if (isInt(nameInput, nameInput.getText())) {
+                label.setText("User is " + nameInput.getText());
+                nameInput.clear();
+            } else {
+                label.setText("User is " + nameInput.getText());
+            }
 
-        // Password Label
-        Label passwordLabel = new Label("Password:");
-        GridPane.setConstraints(passwordLabel, 0, 1);
+        });
 
-        // Password Input
-        TextField passwordInput = new TextField();
-        passwordInput.setPromptText("password");
-        GridPane.setConstraints(passwordInput, 1, 1);
-
-        Button loginButton = new Button("Log In");
-        GridPane.setConstraints(loginButton, 1, 2);
-
-        grid.getChildren().addAll(nameLabel, nameInput, passwordLabel, passwordInput, loginButton);
+        // Layout
+        VBox layout = new VBox(10);
+        layout.setPadding(new Insets(20, 20, 20, 20));
+        layout.getChildren().addAll(nameInput, label, button);
 
 
-        scene = new Scene(grid, 300, 200);
+        scene = new Scene(layout, 300, 250);
         window.setScene(scene);
         window.show();
 
+    }
+
+
+    private boolean isInt(TextField input, String message) {
+        try {
+            int age = Integer.parseInt(input.getText());
+            System.out.println("User is " + age);
+            return true;
+        } catch (NumberFormatException e) {
+            System.out.println("Error: " + message + " is not a number.");
+            return false;
+        }
     }
 
 }
